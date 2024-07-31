732b6be8bf1a98b08afa4fbba764385d6bd2689c
payniexiao <payniexiao@tencent.com>
2024-07-31 11:28:05 +0800

    Fix type

--- components/raftstore-v2/src/operation/command/mod.rs
+++ components/raftstore-v2/src/operation/command/mod.rs
@@ -562,6 +562,7 @@ impl<EK: KvEngine, R: ApplyResReporter> Apply<EK, R> {
                             dr.start_key,
                             dr.end_key,
                             dr.notify_only,
+                            self.use_delete_range,
                         )
                         .await;
                 }
@@ -788,6 +789,7 @@ impl<EK: KvEngine, R: ApplyResReporter> Apply<EK, R> {
                             dr.get_start_key(),
                             dr.get_end_key(),
                             dr.get_notify_only(),
+                            self.use_delete_range,
                         )
                         .await?;
                     }
